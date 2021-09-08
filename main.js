a354dbb338ba4d5bb201c57ef1a778fec7bb2f6e
DavidUrregoz <73442281+DavidUrregoz@users.noreply.github.com>
2021-09-08 10:37:08 -0500

    "Se agrego la clase barra y la funcionalidad de dibujar, las cuales funcionan conjuntamente"

--- main.js
+++ main.js
@@ -13,17 +13,29 @@
     self.Board.prototype = { //Retorna las barras y la pelota
         get elements(){
             var elements=this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 
 })();
 
-
 (function(){
     self.Bar = function(x,y,width,height,board){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.board=board;
+        this.board.bars.push(this);  /// agregar al arreglo las barra
+        this.kind = "rectangle";
+    }
 
+    self.Bar.prototype = {
+        down: function(){
+        },
+        up: function(){
+        }
     }
 })();
 
@@ -35,12 +47,35 @@
         this.board = board;
         this.ctx=canvas.getContext("2d");
     }
+
+    self.BoardView.prototype ={
+        draw: function(){
+            for (var i=this.board.elements.length-1; i>=0;i--){
+                var el= this.board.elements[i];
+                draw(this.ctx,el);
+            }
+        }
+    }
+
+    function draw(ctx,element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            }
+        }
+        
+    }
 })();
 
 window.addEventListener("load",main);
 
 function main(){
     var board = new Board(800,400);
+    var bar1 = new Bar(40,100,20,100,board);
+    var bar2 = new Bar(740,100,20,100,board);
     var canvas = document.getElementById('canvas');
     var boar_view = new BoardView(canvas,board);
+    boar_view.draw();
 }
