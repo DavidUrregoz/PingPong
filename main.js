d8475dbe44ca0fafed2c47133e87c7da06faa13c
DavidUrregoz <73442281+DavidUrregoz@users.noreply.github.com>
2021-09-08 15:36:02 -0500

    Se agrego el sistema de colisiones con un un pequenio error en la barra derecha

--- main.js
+++ main.js
@@ -31,7 +31,10 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;//Sentido de movimiento
-
+        this.bounce_angle = 0;
+        this.max_bounce_angle= Math.PI/12;
+        this.speed=3;
+        
         board.ball = this;  
         this.kind="circle";
     }
@@ -39,6 +42,26 @@
         move: function(){
             this.x += (this.speed_x *this.direction);
             this.y += (this.speed_y * this.direction );
+        },
+        get width(){
+            return this.radius*2;
+        },
+        get height(){
+            return this.radius*2;
+        },
+        
+        collision: function(bar){//Reacciona a la colision
+            var relative_intersect_y = ( bar.y + (bar.height / 2) ) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * - Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 })();
@@ -90,14 +113,48 @@
             };
         },
 
+        check_collisions: function(){
+            for ( var i = this.board.bars.length - 1 ; i>=0; i--){
+                var bar = this.board.bars[i];
+                if(hit(bar,this.board.ball)){
+                    console.log("hp")
+                    this.board.ball.collision(bar);
+                }
+            };
+        },
+
         play:function(){   
             if(this.board.playing){ //Condicional del pause
                 boar_view.clean();
                 boar_view.draw();   
+                this.check_collisions();
                 this.board.ball.move();
             }            
         }
+    }
 
+    function hit(a,b){ //Revisa si a colisiona con b
+    var hit = false;
+    //Colisiones hirizontales
+    if(b.x + b.width >= a.x && b.x < a.x + a.width){
+        //Colisiona verticales
+        if (b.y + b.height >= a.y && b.y < a.y + a.height) 
+        hit = true;
+    }
+
+  //Colisión de a con b
+    if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+        if (b.y <= a.y && b.y + b.height >= a.y + a.height) 
+            hit = true;
+    }
+
+  //Colision b con a
+    if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+        //Colisiona verticales
+        if (a.y <= b.y && a.y + a.height >= b.y + b.height) 
+            hit = true;
+    }
+    return hit;
     }
 
     function draw(ctx,element){
